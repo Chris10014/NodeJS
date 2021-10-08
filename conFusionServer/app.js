aef29135def3adbbb4d8a29efe3dc1a2aa256edd
Christoph Lansche <christoph.lansche@t-online.de>
2021-10-08 16:47:37 +0200

    conFusionServer: express session part 2

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -15,7 +15,7 @@ var leaderRouter = require('./routes/leaderRouter');
 
 const mongoose = require('mongoose');
 
-const Dishes = require('./models/dishes');
+// const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
@@ -43,41 +43,23 @@ app.use(session({
   })
 );
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 function auth (req, res, next) {
   console.log(req.session);
 
-  if(!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
+  if(!req.session.user) {    
+      var err = new Error("You are not authenticated!")
       err.status = 401;
-      next(err);
-      return;
-    }
-
-    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == "admin" && pass == "password") {
-      req.session.user = "admin"; //res.cookie("user", "admin", { signed: true }); //Set cookie
+      return next(err);
+    } else {
+       if (req.session.user == "authenticated") {  
       next(); // authorized
     } else {
-      var err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      next(err);
-    }
-
-  } else {
-    if(req.session.user === "admin") {
-      next();
-    } else {
       var err = new Error('You are not authenticated!');
-          err.status = 401;
-          next(err);
+        err.status = 401;
+        next(err);
     }
   }  
 }
@@ -86,8 +68,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
